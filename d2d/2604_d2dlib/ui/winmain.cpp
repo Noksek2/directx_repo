d4f8c97d40b3dd9499e786d155183a9297be4adb
Noksek2 <ileum37@gmail.com>
2026-05-01 00:42:31 +0900

    260501 modify ui

--- d2d/2604_d2dlib/ui/winmain.cpp
+++ d2d/2604_d2dlib/ui/winmain.cpp
@@ -1,4 +1,4 @@
-#include <d2d.hpp>
+﻿#include <d2d.hpp>
 #include <App.hpp>
 #include <uxtheme.h>
 #pragma comment(lib, "uxtheme.lib")
@@ -30,7 +30,7 @@ struct WinControlData{
 };
 class IWinControl : public WinControlData {
 public:
-	virtual void OwnerDraw(LPDRAWITEMSTRUCT pDIS, ID2D1SolidColorBrush* m_brush) = 0;
+	virtual void OwnerDraw(LPDRAWITEMSTRUCT pDIS, ID2D1SolidColorBrush* mBrush) = 0;
 };
 class WinButtonControl : public IWinControl{
 public:
@@ -57,7 +57,7 @@ public:
 		mButtonEvent = 0U;
 		return gControlID++;
 	}
-	void OwnerDraw(LPDRAWITEMSTRUCT pDIS, ID2D1SolidColorBrush* m_brush) override;
+	void OwnerDraw(LPDRAWITEMSTRUCT pDIS, ID2D1SolidColorBrush* mBrush) override;
 };
 
 
@@ -70,7 +70,7 @@ public:
 	WinButtonControl* const GetButton(UINT ID) {
 		return &mControls[ID];
 	}
-	ID2D1SolidColorBrush* const GetBrush() { return m_brush.Get(); }
+	ID2D1SolidColorBrush* const GetBrush() { return mBrush.Get(); }
 
 	bool InitRender() override {
 
@@ -79,7 +79,7 @@ public:
 		m_d2d->CreateFontDevice(L"Consolas", 15);
 		m_d2d->CreateFontDevice(L"Consolas", 20);
 
-		HRESULT hr = mDCRT->CreateSolidColorBrush(D2D1::ColorF(0.5, 0.1, 0.2, 0.6), &m_brush);
+		HRESULT hr = mDCRT->CreateSolidColorBrush(D2D1::ColorF(0.5, 0.1, 0.2, 0.6), &mBrush);
 		return (SUCCEEDED(hr));
 	}
 
@@ -94,7 +94,7 @@ public:
 		m_d2d->BeginDraw();
 		m_d2d->Clear(D2D1::ColorF(D2D1::ColorF::White));
 
-		mDC->FillRectangle(D2D1::RectF(50, 50, 150, 150), m_brush.Get());
+		mDC->FillRectangle(D2D1::RectF(50, 50, 150, 150), mBrush.Get());
 
 
 
@@ -127,7 +127,37 @@ public:
 
 		mControls_cnt = 4;
 	}
+	void OnPaintClient(WPARAM wParam) {
+		HDC hdc = (HDC)wParam;
+		if (g_app->m_memDC) {
+			BitBlt(hdc, 0, 0, g_app->m_bufW, g_app->m_bufH,
+				   g_app->m_memDC, 0, 0, SRCCOPY);
+		}
+	}
 	void OnPaint() {
+
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(m_hwnd, &ps);
+		if (!m_memDC) {
+
+			RECT rc; GetClientRect(m_hwnd, &rc);
+			g_app->CreateOffscreenBuffer(hdc, rc.right, rc.bottom);
+
+		}
+		this->DrawBackground();
+		//HBRUSH hbrush = CreateSolidBrush(RGB(230, 230, 230));
+		//RECT rc = { 0, 0, g_app->m_bufW, g_app->m_bufH };
+		//FillRect(g_app->m_memDC, &rc, hbrush);
+		//DeleteObject(hbrush);
+		//
+		HBRUSH hbrush = CreateSolidBrush(RGB(150, 120, 200));
+		HGDIOBJ hprev = SelectObject(m_memDC, hbrush);
+		Ellipse(m_memDC, 10, 10, 100, 100);
+		DeleteObject(hbrush);
+		SelectObject(m_memDC, hprev);
+		BitBlt(hdc, 0, 0,m_bufW, m_bufH,
+			   m_memDC, 0, 0, SRCCOPY);
+		EndPaint(m_hwnd, &ps);
 
 	}
 	void OnCommand(WPARAM wParam) {
@@ -150,7 +180,7 @@ public:
 		D2D* d2d = m_d2d;
 		for (int i = 0; i < mControls_cnt; i++) {
 			if (mControls[i].CID == CID)
-				mControls[i].OwnerDraw(pDIS, m_brush.Get());
+				mControls[i].OwnerDraw(pDIS, mBrush.Get());
 		}
 		if (CID == BTN_1 || CID == BTN_2) { // 버튼 ID 확인
 			
@@ -168,12 +198,44 @@ public:
 			*/
 		}
 	}
+	void CreateOffscreenBuffer(HDC refDC, int w, int h) {
+		if (m_memBMP) {
+			SelectObject(m_memDC, m_oldBMP);
+			DeleteObject(m_memBMP);
+			DeleteDC(m_memDC);
+		}
+		m_memDC = CreateCompatibleDC(refDC);
+		m_memBMP = CreateCompatibleBitmap(refDC, w, h);
+		m_oldBMP = (HBITMAP)SelectObject(m_memDC, m_memBMP);
+		m_bufW = w;
+		m_bufH = h;
+	}
+	void DrawBackground() {
+		if (!m_memDC)return;
+		RECT rc = { 0, 0, m_bufW, m_bufH };
 
+		HBRUSH hbrush = CreateSolidBrush(RGB(230, 230, 230));
+		FillRect(m_memDC, &rc, hbrush);
+		DeleteObject(hbrush);
+
+		//HBRUSH hbrush = CreateSolidBrush(RGB(150, 120, 200));
+		//HGDIOBJ hprev = SelectObject(m_memDC, hbrush);
+		//Ellipse(m_memDC, 10, 10, 100, 100);
+		//DeleteObject(hbrush);
+		//SelectObject(m_memDC, hprev);
+	}
+
+	HDC     m_memDC = nullptr;
 protected:
+	HBITMAP m_memBMP = nullptr;
+	HBITMAP m_oldBMP = nullptr;
+	int     m_bufW = 0;
+	int     m_bufH = 0;
+
 	WinButtonControl mControls[10];
 	int mControls_cnt = 0;
 
-	ComPtr<ID2D1SolidColorBrush> m_brush;
+	ComPtr<ID2D1SolidColorBrush> mBrush;
 };
 
 int WINAPI WinMain(HINSTANCE hin, HINSTANCE, LPSTR, int nCmdShow) {
@@ -229,27 +291,43 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		g_app->OnCommand(wParam);
 		return 0;
 	}
-	case WM_PRINTCLIENT:
+	case WM_PRINTCLIENT: {
+		g_app->OnPaintClient(wParam);
+		
+		return 0;
+	}
 	case WM_PAINT: {
-		PAINTSTRUCT ps;
-		HDC hdc = (msg==WM_PAINT)?BeginPaint(hwnd, &ps):(HDC)wParam;
-		HBRUSH hbrush= CreateSolidBrush(RGB(150, 120, 200));
-		HGDIOBJ hprev = SelectObject(hdc, hbrush);
-		Ellipse(hdc, 10, 10, 100, 100);
-		DeleteObject(hbrush);
-		SelectObject(hdc, hprev);
-		if (msg == WM_PAINT)EndPaint(hwnd, &ps);
-
+		g_app->OnPaint();
 		//ValidateRect(hwnd, NULL);
 		return 0;
 	}
-	case WM_SIZE:
-		if (g_app && wParam != SIZE_MINIMIZED) {
+	case WM_LBUTTONDOWN: {
+		RECT rect;
+		char buf[128];
+		GetClientRect(hwnd, &rect);
+		sprintf_s(buf, 128, "(%d,%d,%d,%d)", rect.left, rect.right, rect.top, rect.bottom);
+		SetWindowTextA(hwnd,buf);
+		return 0;
+	}
+					   
+	case WM_SIZE: {
+		if (wParam != SIZE_MINIMIZED) {
+			int w = (int)LOWORD(lParam);
+			int h = (int)HIWORD(lParam);
+			HDC hdc = GetDC(hwnd);
+			g_app->CreateOffscreenBuffer(hdc, w, h);
+			g_app->DrawBackground();          // ★ 버퍼에 배경 즉시 그리기
+			ReleaseDC(hwnd, hdc);
+			// g_app->ResizeScreen(w, h);     // D3D 리사이즈도 여기서
+		}
+		
+		/*if (g_app && wParam != SIZE_MINIMIZED) {
 			int newWidth = (int)LOWORD(lParam);
 			int newHeight = (int)HIWORD(lParam);
 			g_app->ResizeScreen(newWidth, newHeight);
-		}
+		}*/
 		return 0;
+	}
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
@@ -258,7 +336,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 
-void WinButtonControl::OwnerDraw(LPDRAWITEMSTRUCT pDIS, ID2D1SolidColorBrush* m_brush) {
+void WinButtonControl::OwnerDraw(LPDRAWITEMSTRUCT pDIS, ID2D1SolidColorBrush* mBrush) {
 
 
 	D2D* d2d = g_app->GetD2D();
@@ -269,70 +347,85 @@ void WinButtonControl::OwnerDraw(LPDRAWITEMSTRUCT pDIS, ID2D1SolidColorBrush* m_
 	UINT state = pDIS->itemState;
 	HWND hParent = GetParent(pDIS->hwndItem);
 
-	
 
-
-	SendMessage((pDIS->hwndItem), WM_ERASEBKGND, (WPARAM)hdc, 0);
-	
-	// 1. 부모의 배경을 버튼 DC로 복사 (좌표 보정 필요)
-	POINT pt = {0, 0};
-	// 버튼의 (0,0) 좌표가 부모 기준으로는 어디인지 계산
+	// ★ 버튼 위치를 부모 기준 좌표로 변환
+	POINT pt = { 0, 0 };
 	MapWindowPoints(pDIS->hwndItem, GetParent(pDIS->hwndItem), &pt, 1);
 
+
+	if (g_app->m_memDC) {
+		BitBlt(hdc,
+			   rect.left, rect.top,               // 목적지: 버튼 DC의 (0,0)
+			   rect.right - rect.left,
+			   rect.bottom - rect.top,
+			   g_app->m_memDC,
+			   pt.x + rect.left,                  // 원본: 부모 기준 버튼 위치
+			   pt.y + rect.top,
+			   SRCCOPY);
+	}
+
+
+	//SendMessage((pDIS->hwndItem), WM_ERASEBKGND, (WPARAM)hdc, 0);
+	
+	// 1. 부모의 배경을 버튼 DC로 복사 (좌표 보정 필요)
+	//POINT pt = {0, 0};
+	// 버튼의 (0,0) 좌표가 부모 기준으로는 어디인지 계산
+	//MapWindowPoints(pDIS->hwndItem, GetParent(pDIS->hwndItem), &pt, 1);
+
 	// DC의 원점을 부모 기준으로 옮겨서 부모가 "내 위치"에 그리게 함
-	POINT oldOrg;
-	OffsetWindowOrgEx(hdc, pt.x, pt.y, &oldOrg);
-
-	// ★ 대상은 반드시 GetParent(...)여야 함
-	SendMessage(hParent, WM_PRINTCLIENT, (WPARAM)hdc, PRF_CLIENT);
-
-	// 원상 복구
-	SetWindowOrgEx(hdc, oldOrg.x, oldOrg.y, NULL);
+	//POINT oldOrg;
+	//OffsetWindowOrgEx(hdc, pt.x, pt.y, &oldOrg);
+	//
+	//// ★ 대상은 반드시 GetParent(...)여야 함
+	//SendMessage(hParent, WM_PRINTCLIENT, (WPARAM)hdc, PRF_CLIENT);
+	//
+	//// 원상 복구
+	//SetWindowOrgEx(hdc, oldOrg.x, oldOrg.y, NULL);
 
 	
 	//SendMessage(GetParent(pDIS->hwndItem), WM_PRINTCLIENT, (WPARAM)hdc, PRF_CLIENT);
 	d2d->mDCRT->BindDC(hdc, &rect);
 	d2d->mDCRT->BeginDraw();
-	d2d->mDCRT->Clear(D2D1::ColorF(D2D1::ColorF::White));
+	//d2d->mDCRT->Clear(D2D1::ColorF(D2D1::ColorF::White));
 
 	// 2. WPF 느낌의 둥근 사각형 그리기
 	D2D1_RECT_F rt = D2D1::RectF(rect.left + 2, rect.top + 2, rect.right - 2, rect.bottom - 2);
 	D2D1_ROUNDED_RECT roundedRect = D2D1::RoundedRect(
 		rt,
-		5.0f, 5.0f);
+		4.0f, 4.0f);
 	D2D1_RECT_F layoutRect = D2D1::RectF(
 		0, 0,
 		(float)(rect.right - rect.left),
 		(float)(rect.bottom - rect.top)
 	);
 	const TCHAR* text = mText;
-	ID2D1Brush* b = m_brush;
+	ID2D1Brush* b = mBrush;
 	
 	if ((state & ODS_SELECTED)) {
 		mButtonEvent = BE_Pressed;
-		m_brush->SetColor(D2D1::ColorF(mPressColor & 0xFFFFFF, (float)(mPressColor >> 24)/255.f));
+		mBrush->SetColor(D2D1::ColorF(mPressColor & 0xFFFFFF, (float)(mPressColor >> 24)/255.f));
 	}
 	else if (mButtonEvent & BE_Hovered) {
-		m_brush->SetColor(D2D1::ColorF(mHoverColor & 0xFFFFFF, (float)(mHoverColor >> 24) / 255.f));
+		mBrush->SetColor(D2D1::ColorF(mHoverColor & 0xFFFFFF, (float)(mHoverColor >> 24) / 255.f));
 	}
 	else {
 		mButtonEvent &= ~(0b11U);
-		m_brush->SetColor(D2D1::ColorF(D2D1::ColorF::White, 0.5f));
+		mBrush->SetColor(D2D1::ColorF(D2D1::ColorF::White, 0.5f));
 	}
-	d2d->mDCRT->FillRoundedRectangle(roundedRect, m_brush);
-	//d2d->mDCRT->FillRectangle(rt, m_brush);
-	m_brush->SetColor(D2D1::ColorF(D2D1::ColorF::Black,0.5f));
-	d2d->mDCRT->DrawRoundedRectangle(roundedRect, m_brush,0.3f);
+	d2d->mDCRT->FillRoundedRectangle(roundedRect, mBrush);
+	//d2d->mDCRT->FillRectangle(rt, mBrush);
+	mBrush->SetColor(D2D1::ColorF(D2D1::ColorF::Black,0.8f));
+	d2d->mDCRT->DrawRoundedRectangle(roundedRect, mBrush,0.2f);
 
-	//d2d->mDCRT->DrawRectangle(rt, m_brush);
-	//m_brush->SetColor(D2D1::ColorF(D2D1::ColorF::Black));
+	//d2d->mDCRT->DrawRectangle(rt, mBrush);
+	//mBrush->SetColor(D2D1::ColorF(D2D1::ColorF::Black));
 
 	d2d->mDCRT->DrawTextW(
 		text,
 		(UINT32)wcslen(text),
 		d2d->mDWTextFormat[0],
 		layoutRect,
-		m_brush
+		mBrush
 	);
 
 	d2d->mDCRT->EndDraw();
@@ -371,6 +464,7 @@ LRESULT CALLBACK ButtonSubclassProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM 
 	}break;
 	case WM_MOUSEMOVE:
 	{
+		if (Btn->mButtonEvent & WinButtonControl::BE_Pressed) return 0;
 		auto isHover = Btn->mButtonEvent & WinButtonControl::BE_Hovered;
 		if (!isHover) {
 			Btn->mButtonEvent |= WinButtonControl::BE_Hovered;
@@ -386,9 +480,7 @@ LRESULT CALLBACK ButtonSubclassProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM 
 		}
 		break;
 	}
-	case WM_ERASEBKGND:
-		// 0이 아닌 값을 반환하면 시스템은 배경을 다 지운 것으로 간주하고 아무것도 안 합니다.
-		return 1;
+	
 	case WM_MOUSELEAVE:
 	{
 		auto isHover = Btn->mButtonEvent & WinButtonControl::BE_Hovered;
@@ -406,4 +498,3 @@ LRESULT CALLBACK ButtonSubclassProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM 
 	//현재 처리하지 못한 메세지 (원래 버튼 로직, 메세지)는 돌려보냄
 	return DefSubclassProc(hWnd, uMsg, wParam, lParam);
 }
-
